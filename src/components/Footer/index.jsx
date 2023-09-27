53b9ff4dcc9b3cd8eafade25559be75e69bbe362
unknown <abu040810@gmail.com>
2023-09-27 18:03:45 +0500

    ok

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -11,18 +11,18 @@ import Github from "../../assets/icons/github.svg";
 const Footer = () => {
     return (
         <footer className="bg-[#FFFFFF] text-gray-300 py-2 px-2 md:px-8">
-            <div className="container mx-auto block items-center md:px-[96px] md:py-[80px]">
+            <div className="container mx-auto md:p-[100px] block items-center md:px-[96px] md:py-[80px]">
 
-                <div className="block dark-[#1F2937] pt-[40px] md:p-[100px] items-center ">
-                    <button className='rounded-xl  text-[#4B5563] mb-6 ml-[140px] md:ml-[500px]  font-medium text-base font-[Inter] bg-[#E5E7EB] py-2 px-5'>Get in touch</button>
-                    <p className=" dark:text-white font-normal  max-w-[570px]  text-center  font-[Inter]  mb-2  text-xl text-gray-600">
+                <div className="w-full block items-centerq dark-[#1F2937] pt-[40px] md:p-[100px] items-center ">
+                    <button className='rounded-xl  text-[#4B5563] mb-6 ml-[140px] md:ml-[300px]  font-medium text-base font-[Inter] bg-[#E5E7EB] py-2 px-5'>Get in touch</button>
+                    <p className=" dark:text-white font-normal  max-w-[570px]  text-center  font-[Inter]  mb-2 ml-[100px] text-xl text-gray-600">
                         Imom Buxoriy bobomizning “Al Jome as sahih” kitobining <br />eng birinchi hadisi shu hadis bilan boshlangan: “Innamal a’malu  <br />bin niyyati” ya’ni, “Barcha amallar niyatga bog’liq”.
                     </p>
 
 
                 </div>
 
-                <div className="block  ml-[0px] md:ml-[360px] p-4 mt-4 md:mt-[100px]  items-center ">
+                <div className="block  ml-[0px] md:ml-[250px] p-4 mt-4 md:mt-[-100px]  items-center ">
                     <div className="flex gap-x-5  items-center  p-2">
                         <img src={Email} alt="Email icon" />
                         <p className='font-semibold text-center font-[Inter]   text-[#111827] text-lg md:text-3xl'>khodievm@gmail.com</p>
--- src/components/Layout/Work/index.jsx
+++ src/components/Layout/Work/index.jsx
@@ -12,8 +12,8 @@ const index = () => {
                     <button className='rounded-xl  text-[#4B5563] mb-6 ml-[150px] md:ml-[500px]  font-medium text-base font-[Inter] bg-[#E5E7EB] py-2 px-5'>Work</button>
 
                     <h3 className='font-normal text-center md:mr-[80px] font-[Inter]  mb-5  text-xl text-[#4B5563 ]'>Some of the noteworthy projects I have built:</h3>
-                    <div className="past  md:px-[130px] py-[20px] block ">
-                        <div className="uzumm max-w-[700px] block  bg-[#FFFFFF] r md:p-8 md:flex md:gap-x-[130px]  flex-row  h-fit    gap-x-12 rounded-xl border-2">
+                    <div className="past  md:px-[130px] py-[20px] md:block ">
+                        <div className="uzumm max-w-full block  bg-[#FFFFFF] r md:p-8 md:flex md:gap-x-[130px]  flex-row  h-fit    gap-x-12 rounded-xl border-2">
                             <div className="img bg-[#F3F4F6] pt-24 p-3 ">
                                 <img src={img} alt="" />
                             </div>
@@ -70,7 +70,7 @@ const index = () => {
 
                         </div>
 
-                        <div className="uzumnasiyauzumm mt-5 max-w-[500px] block  bg-[#FFFFFF] r md:p-8 md:flex md:gap-x-[130px]  flex-row  h-fit    gap-x-12 rounded-xl border-2">
+                        <div className="uzumnasiyauzumm mt-5 max-w-full block  bg-[#FFFFFF] r md:p-8 md:flex md:gap-x-[130px]  flex-row  h-fit    gap-x-12 rounded-xl border-2">
 
                             <div className="uzumnasiya_texts  mt-4 pt-2 flex-row  md:gap-y-1 md:pt-[15px] md:px-12 block  ">
                                 <h2 className='text-xl text-[#111827]  font-[Inter] font-bold mt-[30px] mb-3'>Uzum nasiya </h2>
@@ -128,7 +128,7 @@ const index = () => {
 
                         </div>
 
-                        <div className="Camblycom  max-w-[500px] block  bg-[#FFFFFF] mt-4 md:p-8 md:flex md:gap-x-[130px]  flex-row  h-fit    gap-x-12 rounded-xl border-2">
+                        <div className="Camblycom  max-w-full block  bg-[#FFFFFF] mt-4 md:p-8 md:flex md:gap-x-[130px]  flex-row  h-fit    gap-x-12 rounded-xl border-2">
                             <div className="img bg-[#F3F4F6]   px-6 pt-16 h-[480px] ">
                                 <img src={cambly} alt="" />
                             </div>
